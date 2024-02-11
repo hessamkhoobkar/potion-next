960d1fee5b963362fd0bd22f7f026892e9b66969
hessam_khoobkar <amirhessam.dev@gmail.com>
2024-02-11 22:00:38 +0330

    add theme toggle to app

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { ModeToggle } from "@/components/ui/ModeToggle";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Click me</Button>
+      <div className="flex gap-4">
+        <Button>Click me</Button>
+        <ModeToggle />
+      </div>
     </main>
   );
 }
